7b49c9ff72b03571ad33bf84d097a8c6c692d3f6
firvain <etsipis@terracognita.eu>
2019-05-09 15:32:37 +0300

    history at server.js

--- server.js
+++ server.js
@@ -3,8 +3,16 @@
  */
 const express = require("express");
 const serveStatic = require("serve-static");
+const history = require("connect-history-api-fallback");
 const path = require("path");
 const app = express();
+
 app.use(serveStatic(path.join(__dirname, "dist")));
+app.use(
+  history({
+    disableDotRule: true,
+    verbose: true
+  })
+);
 const port = process.env.PORT || 80;
 app.listen(port);
